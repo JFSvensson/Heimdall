1deac13783f983d0942e7c2fb6307f7e9291c9d8
Fredrik Svensson <fs222id@student.lnu.se>
2024-05-14 21:35:18 +0200

    Add DHT22 temperature and humidity sensor functionality

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 from machine import Pin
 from time import sleep
 
-pin = Pin("LED", Pin.OUT)
+import dht
+
+# pin = Pin("LED", Pin.OUT)
+# while True:
+#   pin.toggle()
+#   sleep(1)
+
+sensor = dht.DHT22(Pin(22))
+
 while True:
-  pin.toggle()
-  sleep(1)
+  try:
+    sensor.measure()
+    sleep(2)
+    temp = sensor.temperature()
+    hum = sensor.humidity()
+
+    print('Temperature: C', temp)
+
+    print('Humidity:', hum)
+  except OSError as e:
+    print('Failed to read sensor.')
+    break
+  except Exception as e:
+    print('An error occurred:', e)
+  
