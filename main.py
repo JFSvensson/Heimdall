73dd518639d221ac61c8eca8ffac605eaca94b01
Fredrik Svensson <fs222id@student.lnu.se>
2024-05-16 14:23:40 +0200

    Install umqtt and got connection to my server

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+import umqtt.simple as mqtt
+
 from machine import Pin
 from time import sleep
 
 import dht
+
+# MQTT inställningar
+MQTT_BROKER = '185.189.49.210'
+MQTT_PORT = 1883
+MQTT_TOPIC = 'test/iot'
+MQTT_USERNAME = 'iot_project'
+MQTT_PASSWORD = '800grader'
+
+# Initiera MQTT klient
+client = mqtt.MQTTClient('client_id', MQTT_BROKER, port=MQTT_PORT, user=MQTT_USERNAME, password=MQTT_PASSWORD)
+
+# Anslut till MQTT broker
+client.connect()
 
 ledPin = Pin("LED", Pin.OUT)
 sensor = dht.DHT22(Pin(22))
@@ -14,9 +29,17 @@ while True:
     temp = sensor.temperature()
     hum = sensor.humidity()
 
-    print('Temperature: C', temp)
+    data = {
+            'temperature': temp,
+            'humidity': hum
+        }
+    # Skicka data till MQTT broker
+    client.publish(MQTT_TOPIC, str(data))
 
-    print('Humidity:', hum)
+    print('Published:', data)
+    # print('Temperature: C', temp)
+    # print('Humidity:', hum)
+
   except OSError as e:
     print('Failed to read sensor.')
     break
